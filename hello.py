62e16379532e87d20d430e94e1ab5075b73f1e07
2601547@sit.singaporetech.edu.sg <2601547@sit.singaporetech.edu.sg>
2026-09-04 11:00:02 +0800

    First Post

--- hello.py
+++ hello.py
@@ -2,3 +2,21 @@ print("=====================================")
 print("Welcome here")
 print("My first post!")
 print("=====================================")
+
+
+username = "Cool_Creator"
+bio = "Fun Blogger"
+followers = 100
+
+followers += 50  # Increase followers by 50
+print("Day 1:", followers)
+
+followers += 30  # Increase followers by 30
+print("Day 2:", followers)
+
+followers -= 10  # Decrease followers by 10
+print("Day 3:", followers)
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
