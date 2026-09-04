758bc052e027e8f54de3763e7ca8f8e944d34da4
2601547@sit.singaporetech.edu.sg <2601547@sit.singaporetech.edu.sg>
2026-09-04 11:04:39 +0800

    First Post

--- hello.py
+++ hello.py
@@ -4,19 +4,12 @@ print("My first post!")
 print("=====================================")
 
 
-username = "Cool_Creator"
-bio = "Fun Blogger"
-followers = 100
+username = input("Enter your username: ")
+age = input("Enter your age: ")
+category = input("Enter your category: ")
 
-followers += 50  # Increase followers by 50
-print("Day 1:", followers)
-
-followers += 30  # Increase followers by 30
-print("Day 2:", followers)
-
-followers -= 10  # Decrease followers by 10
-print("Day 3:", followers)
-
+print("\nInstagram Profile")
+print("====================")
 print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+print("Age:", age)
+print("Category:", category)
