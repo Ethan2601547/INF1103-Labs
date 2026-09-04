e1e521b1ccab07ac64d09b4557ab4766567ba5ff
2601547@sit.singaporetech.edu.sg <2601547@sit.singaporetech.edu.sg>
2026-09-04 11:09:48 +0800

    First Post

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ print("=====================================")
 
 
 username = input("Enter your username: ")
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
 category = input("Enter your category: ")
 
 print("\nInstagram Profile")
@@ -13,3 +13,7 @@ print("====================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+
+
+if age>40 and category == "fun":
+    print("You are old, what is fun for you???")
